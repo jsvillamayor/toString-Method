fff0f7f47c82a1d9057d55634f15082a2193f292
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 19:44:27 -0700

    Add files via upload

---
+++ toString Method/src/Car.java
@@ -0,0 +1,17 @@
+
+public class Car {
+
+	String make = "Ford";
+	String model = "Mustang";
+	String color = "red";
+	int year = 2022;
+	
+	public String toString() {
+		
+		String myString = make + "\n" + model + "\n" + color + "\n" + year;
+		
+		return myString;
+		
+	}
+	
+}
